ff9d414cfe187ab05e2dda670fb9ce7893bb8c3f
Siddhant Jain <mail.siddhantjain19@gmail.com>
2020-04-20 00:54:13 +0530

    added ipv4 and ipv6 flags

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,11 +1,47 @@
 package main
 
 import (
-  "os"
+	"log"
+	"os"
+	"fmt"
 
-  "github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"
 )
 
-func main() {
-  (&cli.App{}).Run(os.Args)
+var (
+	ipv4 bool
+	ipv6 bool
+)
+
+
+func main(){
+	app := &cli.App{
+		Flags: []cli.Flag {
+			&cli.BoolFlag{ // ipv4 flag
+				Name: "4",
+				Value: true,
+				Usage: "Use IPv4 only",
+				Destination: &ipv4,
+				Required: false,
+			},
+			&cli.BoolFlag{ // ipv6 flag
+				Name: "6",
+				Value: false,  // By default ipv6 is not set
+				Usage: "Use IPv6 only",
+				Destination: &ipv6,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			if ipv6 == true {
+				ipv4 = false // overides behaviour
+			}else{
+
+			}
+			return nil
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
