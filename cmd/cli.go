c764712c566821ff2a87525a2450ff4835d3aa6b
Siddhant Jain <mail.siddhantjain19@gmail.com>
2020-04-24 12:02:09 +0530

    basic code for pinging localhost

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,47 +1,98 @@
 package main
 
 import (
-	"log"
 	"os"
 	"fmt"
+	"flag"
+	"net"
 
-	"github.com/urfave/cli/v2"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
-var (
-	ipv4 bool
-	ipv6 bool
-)
+var usage = `Usage: ping host`
 
-
-func main(){
-	app := &cli.App{
-		Flags: []cli.Flag {
-			&cli.BoolFlag{ // ipv4 flag
-				Name: "4",
-				Value: true,
-				Usage: "Use IPv4 only",
-				Destination: &ipv4,
-				Required: false,
-			},
-			&cli.BoolFlag{ // ipv6 flag
-				Name: "6",
-				Value: false,  // By default ipv6 is not set
-				Usage: "Use IPv6 only",
-				Destination: &ipv6,
-			},
-		},
-		Action: func(c *cli.Context) error {
-			if ipv6 == true {
-				ipv4 = false // overides behaviour
-			}else{
-
-			}
-			return nil
-		},
-	}
-	err := app.Run(os.Args)
+func checkErr(err error) {
 	if err != nil {
-		log.Fatal(err)
+		fmt.Print(err)
+		os.Exit(1)
 	}
 }
+
+func main(){
+	// all flags
+	sudo := flag.Bool("sudo", false, "")
+	flag.Usage = func () {
+		fmt.Println(usage)
+	}
+	flag.Parse()
+	fmt.Println( *sudo, flag.NArg())
+
+	if flag.NArg() == 0{
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// resolve the host
+	host := flag.Arg(0)
+	resolvedHost, err := net.ResolveIPAddr("ip" ,host)
+	checkErr(err)
+
+	// new connecion
+	c, err := icmp.ListenPacket("udp", "0.0.0.0")
+	checkErr(err)
+
+	// var c icmp.PacketConn
+
+	// new message
+	message := icmp.Message{
+		Type: ipv4.ICMPTypeEcho,
+		Code: 8,
+		Checksum: 1234,
+		Body: &icmp.Echo{
+			ID: os.Getpid(),
+			Seq: 1,
+			Data: []byte("You there?"),
+		},
+	}
+	binaryMessage, err := message.Marshal(nil)
+	checkErr(err)
+	
+	// send message
+	_,err = c.WriteTo(binaryMessage, &net.UDPAddr{
+		IP: resolvedHost.IP,
+		Zone: resolvedHost.Zone,
+	})
+	checkErr(err)
+
+
+	// read message
+	rb := make([]byte, 1500)
+	n, peer, err := c.ReadFrom(rb)
+	checkErr(err)
+
+	rm, err := icmp.ParseMessage(58, rb[:n])
+	checkErr(err)
+
+	switch rm.Type {
+	case ipv4.ICMPTypeEchoReply:
+		fmt.Printf("got reflection from %v", peer)
+	default:
+		fmt.Printf("got %+v; want echo reply", rm)
+
+	}
+
+}
+
+// TODO
+// 1. resolve hostname, ip
+// 2. icmp request
+// 3. inturrupt
+
+// Understanding
+// open a raw socket to destination, while true or no inturrupt
+// package echo request
+// send,  with data field = timestamp
+// filter out echo replies from incoming icmp messages
+// filter on basis of Process id
+// get rtt time
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/SiddhantJ19/Ping-Go
 
 go 1.14
 
-require github.com/urfave/cli/v2 v2.2.0
+require (
+	github.com/urfave/cli/v2 v2.2.0
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
+)
